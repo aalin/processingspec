3f2046e1aa4feca67d1812d10b80304ec60bfedb
Andreas Alin <andreas.alin@gmail.com>
2010-01-28 20:50:36 +0100

    box thing test

--- lib/processing_formatter.rb
+++ lib/processing_formatter.rb
@@ -5,6 +5,35 @@ require 'spec/runner/formatter/base_formatter'
 module Processing ; SKETCH_PATH = __FILE__ ; end
 
 class SpecSketch < Processing::App
+  load_library :opengl
+
+  class Cube
+    attr_accessor :status
+    attr_accessor :rotation
+
+    def initialize(rotation = 0.0)
+      @rotation = rotation
+    end
+
+    def draw
+      case self.status
+      when :passed
+        fill(0.0, 1.0, 0.0)
+      when :failed
+        fill(1.0, 0.0, 0.0)
+      when :pending
+        fill(1.0, 1.0, 0.0)
+      else
+        fill(0.3, 0.3, 0.3)
+      end
+
+      push_matrix
+        rotate(@rotation)
+        box 40
+      pop_matrix
+    end
+  end
+
   attr_reader :number_of_examples
 
   def setup
@@ -12,12 +41,14 @@ class SpecSketch < Processing::App
     no_stroke
     frame_rate 30
     color_mode RGB, 1
+    render_mode(OPENGL)
 
     reset_number_of_examples!(0)
   end
   
   def reset_number_of_examples!(num)
     @number_of_examples = num
+    puts @number_of_examples
     @results = []
   end
 
@@ -28,25 +59,19 @@ class SpecSketch < Processing::App
   def draw
     background(0)
 
-    if number_of_examples > 0
-      example_width = self.width / number_of_examples.to_f
-
-      number_of_examples.times do |i|
-        case @results[i]
-        when :passed
-          fill(0.0, 1.0, 0.0)
-        when :failed
-          fill(1.0, 0.0, 0.0)
-        when :pending
-          fill(1.0, 1.0, 0.0)
-        else
-          fill(0.3, 0.3, 0.3)
-        end
-
-        x = i * example_width
-        rect(x, 0, example_width, self.height)
-      end
+    lights
+    examples_per_row = Math::sqrt(number_of_examples).ceil
+    puts number_of_examples
+    number_of_examples.times do |i|
+      x = (i % examples_per_row) * self.width / examples_per_row.to_f
+      y = (i / examples_per_row) * self.height / examples_per_row.to_f
+      puts x
+      push_matrix
+        translate(x, y)
+        Cube.new(0.0).draw 
+      pop_matrix
     end
+
   end
 end
 
